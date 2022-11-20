d9f345e3ec967634b2f2ce3779a4287292bd399d
mgmakh <75171153+mgmakh@users.noreply.github.com>
2022-11-20 22:57:08 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,5 +1,3 @@
 from math import pi
-
-r = float(input ("Enter radius of circle : "))
-
-print ("Area of the circle is: " + str(pi * r**2))
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
